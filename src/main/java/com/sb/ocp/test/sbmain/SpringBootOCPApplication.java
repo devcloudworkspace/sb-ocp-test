71cf71cc906401c73905c8e8670c87a3be9d52b6
devcloudworkspace <devcloudworkspace@gmail.com>
2026-02-23 18:49:22 +0000

    test commit 2

--- src/main/java/com/sb/ocp/test/sbmain/SpringBootOCPApplication.java
+++ src/main/java/com/sb/ocp/test/sbmain/SpringBootOCPApplication.java
@@ -1,12 +1,19 @@
 package com.sb.ocp.test.sbmain;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootOCPApplication {
 
+	private static Logger LOG = LoggerFactory.getLogger(SpringBootOCPApplication.class);
+
 	public static void main(String[] args) {
+
+		LOG.info("Loaded SpringBootOCPApplication successfully...1");
+
 		SpringApplication.run(SpringBootOCPApplication.class, args);
 	}
 
